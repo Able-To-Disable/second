2bd94d1b5d1834ad96668329f4b0dfc3b04ef605
Nail` <162365461+Able-To-Disable@users.noreply.github.com>
2024-07-25 14:12:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 
 
+#создаем класс Node, который представляет узел в двусвязном списке. Узел содержит ключ, указатель на следующий узел и указатель на предыдущий узел.
+
 @dataclass
 class Node:
     def __init__(self, key):
@@ -8,19 +10,22 @@ class Node:
         self.next = None
         self.prev = None
 
-
+#создаем класс LinkedList, который представляет сам список
 class LinkedList:
 
+#создается класс LinkedList, который представляет сам список
     def __init__(self):
         self.head = None
 
+    #функция для вставки нового узла начало списка
     def insert_at_head(self, key):
         new_node = Node(key)
         new_node.next = self.head
         if self.head is not None:
             self.head.prev = new_node
         self.head = new_node
-
+        
+#функция для печати списка
     def print_list(self):
         current = self.head
         while current is not None:
@@ -28,6 +33,7 @@ class LinkedList:
             current = current.next
 
 
+#создается экземпляр класса LinkedList, добавляются несколько узлов с ключами, для примера взял числа(3, 11 и 7), и затем вызывается метод print_list для печати содержимого списка.
 linked_list = LinkedList()
 linked_list.insert_at_head(3)
 linked_list.insert_at_head(11)
